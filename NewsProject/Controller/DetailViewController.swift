b4c22508fc78d09eeb6493cd790956e9c4d027a9
Chayanant <chayanan@magicboxasia.com>
2022-10-06 16:03:29 +0700

    Change DetailController to UIKit

--- NewsProject/Controller/DetailViewController.swift
+++ NewsProject/Controller/DetailViewController.swift
@@ -11,11 +11,76 @@ import SafariServices
 
 class DetailViewController: UIViewController {
     
-    @IBOutlet weak var imageArticle: UIImageView!
-    @IBOutlet weak var titleArticle: UILabel!
-    @IBOutlet weak var descriptionArticle: UILabel!
-    @IBOutlet weak var dateArticle: UILabel!
-    @IBOutlet weak var sourceButton: UIButton!
+    private let imageArticle: UIImageView! = {
+        let imageArticle = UIImageView()
+        
+        imageArticle.translatesAutoresizingMaskIntoConstraints = false
+        imageArticle.contentMode = .scaleToFill
+        imageArticle.backgroundColor = .red
+        
+        return imageArticle
+    }()
+    
+    private let scrollView: UIScrollView! = {
+        let scrollView = UIScrollView()
+        
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.backgroundColor = .white
+        
+       return scrollView
+    }()
+    
+    private let stackView: UIStackView = {
+        let stackView = UIStackView()
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.spacing = 15
+        
+        return stackView
+    }()
+    
+    private let titleArticle: UILabel! = {
+        let titleArticle = UILabel()
+        
+        titleArticle.numberOfLines = 0
+        titleArticle.sizeToFit()
+        titleArticle.translatesAutoresizingMaskIntoConstraints = false
+        
+        return titleArticle
+    }()
+    
+    private let descriptionArticle: UILabel! = {
+        let descriptionArticle = UILabel()
+        
+        descriptionArticle.numberOfLines = 0
+        descriptionArticle.sizeToFit()
+        descriptionArticle.translatesAutoresizingMaskIntoConstraints = false
+        
+        return descriptionArticle
+    }()
+    
+    private let dateArticle: UILabel! = {
+        let dateArticle = UILabel()
+        
+        dateArticle.translatesAutoresizingMaskIntoConstraints = false
+        dateArticle.numberOfLines = 1
+        dateArticle.sizeToFit()
+        
+        return dateArticle
+    }()
+    
+    private let sourceArticle: UIButton! = {
+        let sourceArticle = UIButton()
+        
+        sourceArticle.setTitle("Source", for: .normal)
+        sourceArticle.translatesAutoresizingMaskIntoConstraints = false
+        sourceArticle.titleLabel?.font = .systemFont(ofSize: 24, weight: .bold)
+        sourceArticle.sizeToFit()
+        sourceArticle.backgroundColor = .cyan
+        
+        return sourceArticle
+    }()
     
     var titleData: String?
     var descriptionData: String?
@@ -25,10 +90,38 @@ class DetailViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.addSubview(imageArticle)
+        view.addSubview(scrollView)
+        scrollView.addSubview(stackView)
+        stackView.addArrangedSubview(titleArticle)
+        stackView.addArrangedSubview(descriptionArticle)
+        stackView.addArrangedSubview(dateArticle)
+        stackView.addArrangedSubview(sourceArticle)
+        
         titleArticle.text = titleData
         descriptionArticle.text = descriptionData
-        dateArticle.text = dateData
+        dateArticle.text = displayFormatDate(dateString: dateData)
         renderImage()
+        sourceArticle.addTarget(self, action: #selector(onPressSource), for: .touchUpInside)
+        
+        NSLayoutConstraint.activate([
+            imageArticle.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor,constant: 10),
+            imageArticle.heightAnchor.constraint(equalToConstant: 200),
+            imageArticle.widthAnchor.constraint(equalTo: view.layoutMarginsGuide.widthAnchor),
+            imageArticle.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            scrollView.topAnchor.constraint(equalTo: imageArticle.bottomAnchor, constant: 20),
+            scrollView.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.layoutMarginsGuide.bottomAnchor),
+
+            stackView.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 10),
+            stackView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor,constant: 10),
+            stackView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor,constant: 10),
+            stackView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+            stackView.widthAnchor.constraint(equalTo: scrollView.widthAnchor,constant: -20),
+        ])
     }
     
     func renderImage() {
@@ -46,8 +139,25 @@ class DetailViewController: UIViewController {
         }
     }
     
+    func displayFormatDate (dateString:String?) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "TH")
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+        let date = dateFormatter.date(from: dateString!)
+
+        if let date = date {
+            dateFormatter.dateFormat = "dd MMM yyyy"
+            let resultDate = dateFormatter.string(from: date)
+
+            return resultDate
+        }
+        else {
+            return ""
+        }
+    }
     
-    @IBAction func onPressSource(_ sender: UIButton) {        
+    
+    @objc func onPressSource() {
         if let wrapSourceURL = sourceURLData {
             let safariVC = SFSafariViewController(url: URL(string: wrapSourceURL)!)
             self.present(safariVC, animated: true)
